2816046136fd5733c2b19ab098088d9ea3211699
Medkhat <medkhat.akimbek@gmail.com>
2020-09-22 17:26:43 +0600

    added video slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 function toggleNavbar(selector) {
     document.querySelector(selector).classList.toggle('active');
 }
+
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,  
+        merge: true,
+        responsiveClass: true,
+        video: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1,
+                nav: true
+            },
+            600:{
+                items:1,
+                nav: false
+            },
+            1000:{
+                items:2,
+                nav: true,
+                loop: true
+            }
+        }
+    });
+});
+  
